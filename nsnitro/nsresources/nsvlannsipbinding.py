74d0e2f6cc9f347990be40c442b3e35760553c04
Robison Jacka <robison@packetized.io>
2014-03-27 11:55:15 -0500

    W601: fixed deprecated used of has_key()

--- nsnitro/nsresources/nsvlannsipbinding.py
+++ nsnitro/nsresources/nsvlannsipbinding.py
@@ -23,7 +23,7 @@ class NSVLANNSIPBinding(NSBaseResource):
 
         if not (json_data is None):
             for key in json_data.keys():
-                if self.options.has_key(key):
+                if key in self.options:
                     self.options[key] = json_data[key]
 
         self.resourcetype = NSVLANNSIPBinding.get_resourcetype()
