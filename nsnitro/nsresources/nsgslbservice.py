034551e3207c220ab560fbf4a16838dd2560a429
David Mahler <dave.mahler@gmail.com>
2016-06-01 00:24:57 -0700

    Adding enable, disable, add, delete for gslb services (#106)

--- nsnitro/nsresources/nsgslbservice.py
+++ nsnitro/nsresources/nsgslbservice.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 from nsbaseresource import NSBaseResource
-
+from nsservice import NSService
 
 class NSGSLBService(NSBaseResource):
 
@@ -86,10 +86,63 @@ class NSGSLBService(NSBaseResource):
         """
         return self.options['publicip']
 
+    def set_ip(self, ip):
+        """
+        """
+        self.options['ip'] = ip
+
     def get_ip(self):
         """
         """
         return self.options['ip']
+
+    def set_port(self, port):
+        """
+        """
+        self.options['port'] = port
+
+    def get_port(self):
+        """
+        """
+        return self.options['port']
+
+    def set_state(self, state):
+        """
+        """
+        self.options['state'] = state
+
+    def get_state(self):
+        """
+        """
+        return self.options['state']
+
+    @staticmethod
+    def disable(nitro, service):
+        """
+        Use this API to disable service.
+        """
+        __service = NSService()
+        __service.set_name(service.get_servicename())
+        return __service.perform_operation(nitro, "disable")
+
+    @staticmethod
+    def enable(nitro, service):
+        """
+        Use this API to enable service.
+        """
+        __service = NSService()
+        __service.set_name(service.get_servicename())
+        return __service.perform_operation(nitro, "enable")
+
+    @staticmethod
+    def get(nitro, service):
+        """
+        Use this API to fetch service resource of given name.
+        """
+        __service = NSGSLBService()
+        __service.set_servicename(service.get_servicename())
+        __service.get_resource(nitro, service.get_servicename())
+        return __service
 
     @staticmethod
     def get_all(nitro):
@@ -103,3 +156,27 @@ class NSGSLBService(NSBaseResource):
         for json_gslbvserver in __json_gslbvservers:
             __gslbvservers.append(NSGSLBService(json_gslbvserver))
         return __gslbvservers
+
+    @staticmethod
+    def add(nitro, service):
+        """
+        Use this API to add service.
+        """
+        __service = NSGSLBService()
+        __service.set_ip(service.get_ip())
+        __service.set_servicetype(service.get_servicetype())
+        __service.set_servicename(service.get_servicename())
+        __service.set_port(service.get_port())
+        __service.set_sitename(service.get_sitename())
+        __service.set_state(service.get_state())
+        return __service.add_resource(nitro)
+
+    @staticmethod
+    def delete(nitro, service):
+        """
+        Use this API to delete service of a given name.
+        """
+        __service = NSGSLBService()
+        __service.set_servicename(service.get_servicename())
+        nsresponse = __service.delete_resource(nitro)
+        return nsresponse
